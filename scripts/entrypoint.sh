b53beb19c96dc187ecae8aa1a39ed5d455b2851f
Michel Meyer <michel.meyer@cargomedia.ch>
2017-07-05 11:44:26 +0200

    Add FORCE_SETUP / FORCE_RELOAD env

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,20 @@
-#!/usr/bin/env bash
-set -e
-ACTION="${1:-start}"
+#!/bin/bash -e
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-shift
+ACTION="${1:-start}"
+export FORCE_SETUP=${FORCE_SETUP:-0}
+export FORCE_RELOAD=${FORCE_RELOAD:-0}
+if [ "${FORCE_RELOAD}" == '1' ]; then
+  FORCE_SETUP='1'
+fi
 
-echo "Run ${ACTION} with args:${@}"
+
+shift
+echo "Run ${ACTION}"
+if [ "$#" -ne 0 ]; then
+    echo "- arguments: ${@}"
+fi
+echo "- FORCE_SETUP: ${FORCE_SETUP}"
+echo "- FORCE_RELOAD: ${FORCE_RELOAD}"
+
 ${CURRENT_DIR}/${ACTION}.sh "${@}"
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,13 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash -e
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source ${CURRENT_DIR}/lib.sh
 
 cp ./resources/config/_local.dev.php ./resources/config/local.php
 cp ./resources/config/_local.docker.php ./resources/config/local.docker.php
 
-${CURRENT_DIR}/app-wait-services.sh
-${CURRENT_DIR}/app-setup.sh
+app_wait_services mysql,redis,memcached,mongo,elasticsearch,gearman
+app_setup "${DATA_DIR:-/tmp}/setup.done"
 
+echo 'Start supervisord'
 supervisord -c /etc/supervisor.conf
